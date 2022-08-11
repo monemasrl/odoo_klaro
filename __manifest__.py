9582fc33d907399b873e319bbca7beb71a3258ef
Andrea Bettarini <bettarini@monema.it>
2022-08-11 17:47:24 +0200

    License updated
    initial README.rst

--- __manifest__.py
+++ __manifest__.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
+# MIT License
 #
-#    jeffery CHEN fan<jeffery9@gmail.com>
+# Copyright (c) 2022 Monema S.r.l.
 #
-#    Copyright (c) All rights reserved:
-#        (c) 2017  TM_FULLNAME
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses
-#
-#    Odoo and OpenERP is trademark of Odoo S.A.
-#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 ###############################################################################
 {
     'name': 'Cookie Consent Klaro Integration',
     'summary': 'Cookie Consent Klaro Integration',
-    'version': '1.0',
+    'version': '14.0.1.1.0',
 
     'description': """
 Cookie Consent Klaro Integration.
@@ -35,12 +35,12 @@ Cookie Consent Klaro Integration.
     """,
 
     'author': 'Monema S.r.l.',
-    'maintainer': 'Monema S.r.l.',
-    'contributors': ['Monema S.r.l. <info@monema.it>'],
+    'maintainer': 'Andrea Bettarini',
+    'contributors': ['Andrea Bettarini <bettarini@monema.it>'],
 
     'website': 'https://monema.it/',
 
-    'license': 'AGPL-3',
+    'license': 'Other OSI approved licence',
     'category': 'Uncategorized',
 
     'depends': [
